d2b046687c26ae689042d68a5e27cd2b01599b48
Alexei <zeildeidtv121@gmail.com>
2023-10-01 21:40:52 +0300

    homework

---
+++ calucale.py
@@ -0,0 +1,12 @@
+print('calucale')
+a = int(input('введите число'))
+b = int(input('введите число'))
+c = input('введите символ / +  - * **')
+if c == '+':
+    print('a', '+', 'b', '=', a + b)
+if c == '-':
+    print('a', '-', 'b', '=', a - b)
+if c == '/':
+    print('a', '/', 'b', '=', a / b)
+if c == '*':
+    print('a', '*', 'b', '=', a * b)

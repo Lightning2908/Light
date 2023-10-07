ee02dc97c2bfc99e714485d80d6fe3eeeb463209
Alexei <zeildeidtv121@gmail.com>
2023-10-07 12:00:54 +0300

    Light

--- calucale.py
+++ calucale.py
@@ -1,12 +1,29 @@
-print('calucale')
-a = int(input('введите число'))
-b = int(input('введите число'))
-c = input('введите символ / +  - * **')
-if c == '+':
-    print('a', '+', 'b', '=', a + b)
-if c == '-':
-    print('a', '-', 'b', '=', a - b)
-if c == '/':
-    print('a', '/', 'b', '=', a / b)
-if c == '*':
-    print('a', '*', 'b', '=', a * b)
+# print('calucale')
+# a = int(input('введите число'))
+# b = int(input('введите число'))
+# c = input('введите символ / +  - * **')
+# if c == '+':
+#     print('a', '+', 'b', '=', a + b)
+# if c == '-':
+#     print('a', '-', 'b', '=', a - b)
+# if c == '/':
+#     print('a', '/', 'b', '=', a / b)
+# if c == '*':
+#     print('a', '*', 'b', '=', a * b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

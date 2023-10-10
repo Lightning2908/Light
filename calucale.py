b902f9bdd5052b3faf54cd65397d4474cc787a37
Alexei <zeildeidtv121@gmail.com>
2023-10-10 20:38:52 +0300

    Light

--- calucale.py
+++ calucale.py
@@ -1,15 +1,15 @@
-# print('calucale')
-# a = int(input('введите число'))
-# b = int(input('введите число'))
-# c = input('введите символ / +  - * **')
-# if c == '+':
-#     print('a', '+', 'b', '=', a + b)
-# if c == '-':
-#     print('a', '-', 'b', '=', a - b)
-# if c == '/':
-#     print('a', '/', 'b', '=', a / b)
-# if c == '*':
-#     print('a', '*', 'b', '=', a * b)
+print('calucale')
+a = int(input('введите число'))
+b = int(input('введите число'))
+c = input('введите символ / +  - * ')
+if c == '+':
+    print('a', '+', 'b', '=', a + b)
+if c == '-':
+    print('a', '-', 'b', '=', a - b)
+if c == '/':
+    print('a', '/', 'b', '=', a / b)
+if c == '*':
+    print('a', '*', 'b', '=', a *
 
 
 
@@ -27,3 +27,10 @@
 
 
 
+# shoppinglist = ["яблоко", "молоко", "хлеб", "яйца", "сок"]
+# del shoppinglist[1]
+# shoppinglist[0] = "банан"
+# elements_count = len(shoppinglist)
+# for shopping in shoppinglist:
+#     print(elements_count)
+#     print(shopping)

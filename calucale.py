b842dec4da46b8f6c70f4f6f33e4b708c173a917
Alexei <zeildeidtv121@gmail.com>
2023-12-02 16:03:47 +0300

    Light

--- calucale.py
+++ calucale.py
@@ -45,3 +45,49 @@
 # print(elements_count)
 # for shopping in shoppinglist:
 #    print(shopping)
+
+
+
+
+
+
+
+
+
+#people = int(input())
+#deth = int(input())
+#num = list(range(1, people + 1))
+#while len(num) !=1:
+#    index = deth % len(num)
+#    deth = index
+#    del num[deth]
+#    deth+=2
+#print(num)
+
+
+
+
+# калькулятор на функциях
+# b = input('введите симбвол + , - , * , / ')
+# a = int(input('введите число'))
+# c = int(input('введите число'))
+# def calucale():
+#     if b == "+":
+#         t = a + c
+#     elif b == "-":
+#         t = a - c
+#     elif b == "*":
+#         t = a * c
+#     elif b == "/":
+#         t = a / c
+#     print(f'ответ: {t}')
+# if b == "/" and c == 0:
+#     print(f"Ошибка , на ноль делить нельзя")
+# else:
+#     calucale()
+
+
+
+
+
+
